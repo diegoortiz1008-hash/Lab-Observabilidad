a6772bdc9d90e58e1c64ea097dbd02f6426fc511
diegoortiz1008-hash <diegoortiz1008@gmail.com>
2026-07-05 21:10:57 -0500

    first commit

---
+++ src/test/java/edu/eci/arsw/observability_demo/ObservabilityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.observability_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
